a844ad79d4a80d40487999b35037eed7889bd8fd
ray_wu <ray_wu@uranplus.com>
2024-02-01 15:59:07 +0800

    修改调整

--- app/(main)/projects/page.tsx
+++ app/(main)/projects/page.tsx
@@ -29,7 +29,7 @@ export default function ProjectsPage() {
         </h1>
         <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
           多年来，我一直在做各种各样的小项目，有<b>开源</b>的，有<b>实验</b>
-          的，也有 <b>just for fun </b>
+          的，也有 <b>商业</b>
           的，下面就是我筛选出来我觉得还不错的项目合集，也是我在技术领域中尝试和探索的最好见证。
         </p>
       </header>
